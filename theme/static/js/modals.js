e5317b672aed471b05fb248842fde0d4720a284f
hyi <hongyi@renci.org>
2018-06-12 10:26:09 -0400

    further cleanup to remove rep-resource-to-user-zone

--- theme/static/js/modals.js
+++ theme/static/js/modals.js
@@ -1,33 +1,3 @@
-/**
-* Created by Mauriel on 3/9/2017.
-*/
-
-function rep_res_to_irods_user_zone_ajax_submit(res_id) {
-    setPointerEvents(false);
-    $.ajax({
-        url: "/hsapi/_internal/" + res_id + "/rep-res-bag-to-irods-user-zone/",
-        type: "POST",
-        data: {},
-        success: function(json) {
-            if (json.success) {
-                $("#rep-alert-success").show();
-                $("#rep-status-success").text(json.success);
-            }
-            if (json.error) {
-                $("#rep-alert-error").show();
-                $("#rep-status-error").text(json.error);
-            }
-            $('#rep-resource-to-irods-dialog').modal('hide');
-        },
-        error: function(xhr, errmsg, err) {
-            status_error = xhr.status + ": " + xhr.responseText + ". Error message: " + errmsg;
-            console.log(status_error);
-            $("#rep-status-error").text(status_error);
-            $('#rep-resource-to-irods-dialog').modal('hide');
-        }
-    });
-}
-
 // These event bindings will work even for elements created dynamically
 $(document).on('click', '.btn-unshare-resource', function () {
     var formID = $(this).closest("form").attr("id");
@@ -46,10 +16,6 @@ $(document).on("click", ".btn-change-share-permission", function () {
 
 $(document).ready(function() {
     var resID = $("#resID").val();
-
-    $("#btn-replicate").click(function() {
-        rep_res_to_irods_user_zone_ajax_submit(resID);
-    });
 
     $("#download-bag-btn").click(function() {
         $("#license-agree-dialog-bag").modal('hide');
