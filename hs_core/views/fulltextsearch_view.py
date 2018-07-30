446f658718c8492b80a0e66f559d1c279c9c7669
hyi <hongyi@renci.org>
2018-07-30 18:02:43 -0400

    reduce search result by resource id and filename and add and search query

--- hs_core/views/fulltextsearch_view.py
+++ hs_core/views/fulltextsearch_view.py
@@ -1,5 +1,6 @@
 import os
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import RequestError
 
 from rest_framework import status
 from rest_framework.exceptions import NotFound
@@ -18,58 +19,70 @@ def ftsearchview(request):
     term = request.GET.get('q', '')
 
     response_data = {}
+    response_data["results"] = []
     if term:
-        results = es.search(index="fts_index", doc_type="fts_doc",
-                            body={"query": {"match": {"contents": term}}})
-        response_data["results"] = []
-        hits = results['hits']['hits']
-        res_id_list = []
-        for doc in hits:
-            rid = doc['_source']['guid']
-            if rid in res_id_list:
-                continue
-            try:
-                robj, authorized, _ = authorize(request, rid,
-                                                needed_permission=ACTION_TO_AUTHORIZE.VIEW_RESOURCE,
-                                                raises_exception=False)
-                if not authorized:
-                    # only show resources the user has access to
+        # check if term includes AND bool operator
+        term_split = term.lower().split()
+        if 'and' in term_split:
+            opr_str = 'AND'
+        else:
+            opr_str = 'OR'
+        try:
+            results = es.search(index="fts_index", doc_type="fts_doc",
+                                default_operator=opr_str, q=term,
+                                _source_exclude=['contents'])
+            hits = results['hits']['hits']
+            res_id_list = []
+            for doc in hits:
+                rid = doc['_source']['guid']
+                fname = doc['_source']['filename']
+                id_to_search = rid + '-' + fname
+                if id_to_search  in res_id_list:
                     continue
-            except NotFound:
-               # only show resources hosted by the site
-               continue
-            res_url = robj.get_absolute_url()
-            res_type = robj.resource_type
-            idx = res_type.find('Resource')
-            if idx > 0:
-                res_type = res_type[:idx]
-            res_title = robj.metadata.title.value
-            res_creator = robj.first_creator.name
-            res_create_time = robj.created.strftime('%m-%d-%Y at %I:%M %p')
-            res_update_time = robj.updated.strftime('%m-%d-%Y at %I:%M %p')
-            fname = doc['_source']['filename']
+                try:
+                    robj, authorized, _ = authorize(request, rid,
+                                                    needed_permission=ACTION_TO_AUTHORIZE.VIEW_RESOURCE,
+                                                    raises_exception=False)
+                    if not authorized:
+                        # only show resources the user has access to
+                        continue
+                except NotFound:
+                    # only show resources hosted by the site
+                    continue
+                res_url = robj.get_absolute_url()
+                res_type = robj.resource_type
+                idx = res_type.find('Resource')
+                if idx > 0:
+                    res_type = res_type[:idx]
+                res_title = robj.metadata.title.value
+                res_creator = robj.first_creator.name
+                res_create_time = robj.created.strftime('%m-%d-%Y at %I:%M %p')
+                res_update_time = robj.updated.strftime('%m-%d-%Y at %I:%M %p')
 
-            f_url = ''
-            for f in ResourceFile.objects.filter(object_id=robj.id):
-                name_with_full_path = os.path.join(rid, 'data', fname)
-                if name_with_full_path == f.storage_path:
-                    f_url = get_resource_file_url(f)
-                    break
+                f_url = ''
+                for f in ResourceFile.objects.filter(object_id=robj.id):
+                    name_with_full_path = os.path.join(rid, 'data', fname)
+                    if name_with_full_path == f.storage_path:
+                        f_url = get_resource_file_url(f)
+                        break
 
-            response_data['results'].append({
-                "score": doc['_score'],
-                "filename": fname,
-                'file_url': f_url,
-                "res_type": res_type,
-                "res_url": res_url,
-                'res_title': res_title,
-                'res_creator': res_creator,
-                'res_create_time': res_create_time,
-                'res_update_time': res_update_time
-            })
-            res_id_list.append(rid)
+                response_data['results'].append({
+                    "score": doc['_score'],
+                    "filename": fname,
+                    'file_url': f_url,
+                    "res_type": res_type,
+                    "res_url": res_url,
+                    'res_title': res_title,
+                    'res_creator': res_creator,
+                    'res_create_time': res_create_time,
+                    'res_update_time': res_update_time
+                })
+                res_id_list.append(id_to_search)
 
-        return JsonResponse(response_data, status=status.HTTP_200_OK)
+            return JsonResponse(response_data, status=status.HTTP_200_OK)
+
+        except RequestError as ex:
+            response_data['error'] = ex.info
+            return JsonResponse(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
-        response_data['results'] = []
         return JsonResponse(response_data, status=status.HTTP_400_BAD_REQUEST)
