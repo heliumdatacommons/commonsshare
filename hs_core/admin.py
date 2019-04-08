feb6b9e5b8b6475da620e0af41ef2ff448be8d67
Isma Gilani <igilani@renci.org>
2019-04-08 14:02:08 -0400

    django uprade

--- hs_core/admin.py
+++ hs_core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.gis import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from .models import *
 
@@ -6,5 +7,6 @@ from .models import *
 class InlineResourceFiles(GenericTabularInline):
     model = ResourceFile
 
-
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(GenericResource)
