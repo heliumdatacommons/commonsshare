8141fd8ea2b1237304b86efc01a90ac14f622b99
Isma Gilani <igilani@renci.org>
2019-04-08 13:40:02 -0400

    django upgrade

--- hydroshare/settings.py
+++ hydroshare/settings.py
@@ -491,7 +491,7 @@ except ImportError:
 else:
     set_dynamic_settings(globals())
 
-ACCOUNT_PROFILE_MODEL = "theme.UserProfile"
+ACCOUNTS_PROFILE_MODEL = "theme.UserProfile"
 CRISPY_TEMPLATE_PACK = 'bootstrap'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
